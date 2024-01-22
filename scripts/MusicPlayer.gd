232728dff47db54a5d5c851cff14cefe1a23ca71
Hugo <haaa20@sussex.ac.uk>
2024-01-22 17:03:31 +0000

    Making progress with the fader

--- scripts/MusicPlayer.gd
+++ scripts/MusicPlayer.gd
@@ -16,13 +16,25 @@ func play(id):
 	currently_playing = track
 
 func fade_to(id):
-#	AudioServer.add_bus()
-#	var i = AudioServer.bus_count-1
-#	var original_bus = currently_playing.get_bus()
-#	var new_bus = AudioServer.get_bus_name(AudioServer.bus_count-1)
-#	var new_track = audio_ids[id]
-#
-#	AudioServer.set_bus_send(i, original_bus)
-#	AudioServer.set_bus_volume_db(i, -20)
-#	currently_playing.set_bus(new_bus)
+	# Create a new bus which is sent to the original
+	# Set the new tracks to the original 
+	AudioServer.add_bus()
+	var i = AudioServer.bus_count-1
+	var original_bus = currently_playing.get_bus()
+	var new_bus = AudioServer.get_bus_name(AudioServer.bus_count-1)
+	var new_track = audio_ids[id]
+	AudioServer.set_bus_send(i, original_bus)
+	
+	# Redirect the old tracks to the new bus
+	currently_playing.set_bus(new_bus)
+	
+	# Add an amp effect to fade out the new bus
+	var amp = AudioEffectAmplify.new()
+	AudioServer.add_bus_effect(i, amp)
+	
+	# begin tweening 
+	var fade_tween = get_tree().create_tween()
+	fade_tween.tween_property(amp, "volume_db", -30, 5).as_relative()
+	fade_tween.play()
+	
 	pass
