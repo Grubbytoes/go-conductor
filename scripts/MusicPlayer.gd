b7cbb7e9f23b46727d360d23663212c2f3d87b87
Hugo <haaa20@sussex.ac.uk>
2024-01-22 16:51:15 +0000

    Working on fading, added bus setters and getters

--- scripts/MusicPlayer.gd
+++ scripts/MusicPlayer.gd
@@ -1,6 +1,8 @@
 extends Node
 
 var audio_ids = {}
+var is_playing = false
+var currently_playing = null
 
 func _ready():
 	for child in get_children():
@@ -8,4 +10,19 @@ func _ready():
 
 # Plays the node of the given id
 func play(id):
-	audio_ids[id].play()
+	is_playing = true
+	var track = audio_ids[id]
+	track.play()
+	currently_playing = track
+
+func fade_to(id):
+#	AudioServer.add_bus()
+#	var i = AudioServer.bus_count-1
+#	var original_bus = currently_playing.get_bus()
+#	var new_bus = AudioServer.get_bus_name(AudioServer.bus_count-1)
+#	var new_track = audio_ids[id]
+#
+#	AudioServer.set_bus_send(i, original_bus)
+#	AudioServer.set_bus_volume_db(i, -20)
+#	currently_playing.set_bus(new_bus)
+	pass
--- scripts/level0.gd
+++ scripts/level0.gd
@@ -3,3 +3,7 @@ extends Node2D
 
 func _ready():
 	MusicPlayer.play("tangerine-I")
+
+
+func _on_button_pressed():
+	MusicPlayer.fade_to("spinning out")
